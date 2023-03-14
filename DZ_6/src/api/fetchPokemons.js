42c4c4412509be8c7ac5a453b73e9cf7870afe7b
maksat <118560860+maksatss@users.noreply.github.com>
2023-03-14 23:48:50 +0600

    Add files via upload

---
+++ DZ_6/src/api/fetchPokemons.js
@@ -0,0 +1,32 @@
+import axios from "axios"
+import { sortItem } from "../components/common/helpers"
+
+const baseUrl = 'https://pokeapi.co/api/v2/'
+
+export const fetchPokemons = async (limit, offset) => {
+    try {
+        const { data } = await axios.get(baseUrl + `pokemon/?limit=${limit}&offset=${offset}`)
+        return data
+    } catch (error) {
+        console.log('error');
+    }
+}
+export const fetchPokemonS = async (from, till, type) => {
+    let list = [];
+    for (let i = from; i < till; i++) {
+        const {data} = await axios.get(baseUrl + `pokemon/` + i)
+        list.push(data)
+    }
+    console.log(sortItem(list, 'attach'), 'sortList');
+    return sortItem(list, type)
+}
+
+
+export const getPokemon = async (id) => {
+    try { //помогает улавливать ошибки
+        const { data } = await axios.get(baseUrl + `pokemon/${id}`)
+        return data
+    } catch (error) {
+        console.log('error');
+    }
+}
